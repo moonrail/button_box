e9f5d66929cf1cad36eda77d0905799acae82d34
Jan Gaßner <jan.gassner@plusserver.com>
2024-03-01 01:18:51 +0100

    Initial commit

---
+++ boot.py
@@ -0,0 +1,58 @@
+import storage
+import supervisor
+import usb_cdc
+import usb_hid
+import usb_midi
+
+from board import GP22
+from digitalio import DigitalInOut, Pull
+
+
+supervisor.set_usb_identification(
+    manufacturer='Example',
+    product='Button Box v1'
+)
+
+# Joystick with 64 digital buttons
+JOYSTICK_REPORT_DESCRIPTOR = bytes((
+    0x05, 0x01,  # Usage Page (Generic Desktop Ctrls)
+    0x09, 0x04,  # Usage (Joystick)
+    0xA1, 0x01,  # Collection (Application)
+    0x85, 0x01,  #   Report ID (1)
+    0x05, 0x09,  #   Usage Page (Button)
+    0x19, 0x01,  #   Usage Minimum (Button 1)
+    0x29, 0x40,  #   Usage Maximum (Button 64)
+    0x15, 0x00,  #   Logical Minimum (0), buttons are binary after all
+    0x25, 0x01,  #   Logical Maximum (1), buttons are binary after all
+    0x75, 0x01,  #   Report Size (1), each button is 1 bit
+    0x95, 0x40,  #   Report Count (64), so 64 buttons * 1 bit
+    0x81, 0x02,  #   Input (Data,Var,Abs)
+    0xC0,        # End Collection
+))
+
+joystick = usb_hid.Device(
+    report_descriptor=JOYSTICK_REPORT_DESCRIPTOR,
+    usage_page=0x01,           # Generic Desktop Control
+    usage=0x04,                # Joystick
+    report_ids=(1,),           # Descriptor uses report ID 1.
+    in_report_lengths=(8,),    # This joystick sends 8 bytes in its report.
+    out_report_lengths=(0,),   # It does not receive any reports.
+)
+
+usb_hid.enable(
+    (joystick,)
+)
+
+ESCAPE_BUTTON = DigitalInOut(GP22)
+ESCAPE_BUTTON.pull = Pull.UP
+# Uncomment the following lines to disable serial by default
+# Make sure you've changed the pin of the escape button to your setup and connected a button to it against GND
+# Otherwise you'll not be able to enable serial anymore and have to reflash your board
+# See for more info: https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/circuitpy-midi-serial
+"""
+if ESCAPE_BUTTON.value:
+    # Disables serial & co. if escape button is not pressed.
+    storage.disable_usb_drive()
+    usb_cdc.disable()
+    usb_midi.disable()
+"""
---
+++ joystick.py
@@ -0,0 +1,179 @@
+# SPDX-FileCopyrightText: 2018 Dan Halbert for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`Joystick`
+====================================================
+
+* Author(s): Dan Halbert, Jan Gaßner
+
+Modified version of Dan Halberts example without joystick, with more buttons and different interfaces.
+
+Original version: https://github.com/adafruit/Adafruit_CircuitPython_HID/blob/6.0.3/examples/hid_gamepad.py
+"""
+
+import struct
+import time
+
+from adafruit_hid import find_device
+from usb_hid import Device
+
+
+class Joystick:
+    """
+    Emulates a generic joystick with 64 buttons, numbered 1-64
+    """
+
+    def __init__(self, devices: list[Device] | Device):
+        """
+        Creates a Joystick object that will send USB joystick HID reports
+
+        devices -- a list of devices that include a joystick device or a joystick device
+        itself. A device is any object that implements `send_report()`, `usage_page` and
+        `usage`.
+        """
+        self._device = find_device(devices, usage_page=0x1, usage=0x04)
+
+        # Reuse this bytearray to send reports.
+        # Typically controllers start numbering buttons at 1 rather than 0.
+        # report[0] buttons 1-8 (LSB is button 1)
+        # report[1] buttons 9-16
+        # report[2] buttons 17-24
+        # report[3] buttons 25-32
+        # report[4] buttons 33-40
+        # report[5] buttons 41-48
+        # report[6] buttons 49-56
+        # report[7] buttons 57-64
+        self._report = bytearray(8)
+
+        # Remember the last report as well, so we can avoid sending
+        # duplicate reports.
+        self._last_report = bytearray(8)
+
+        # Store settings separately before putting into report. Saves code
+        # especially for buttons.
+        self._buttons_1_to_32_state = 0
+        self._buttons_33_to_64_state = 0
+
+        # Send an initial report to test if HID device is ready.
+        # If not, wait a bit and try once more.
+        try:
+            self.reset_all()
+        except OSError:
+            time.sleep(1)
+            self.reset_all()
+
+    def _get_state_for_button(self, button: int):
+        """
+        button -- button number to press (starting at 1)
+
+        returns specific button state
+        """
+        if button < 1:
+            raise ValueError(f'Button {button} does not exist - numbering starts at `1`')
+        elif button < 33:
+            return bool((self._buttons_1_to_32_state >> (button - 1)) & 1)
+        elif button < 65:
+            return bool((self._buttons_33_to_64_state >> (button - 33)) & 1)
+        else:
+            raise ValueError(f'Button {button} does not exist')
+
+    def _set_state_for_button(self, button: int, value: bool | int, send=False):
+        """
+        sets specific button state
+
+        button -- button number to press (starting at 1)
+
+        value -- value to set state to
+
+        send -- if report should be send
+        """
+        if button < 1:
+            raise ValueError(f'Button {button} does not exist - numbering starts at `1`')
+        elif button < 33:
+            if value:
+                self._buttons_1_to_32_state |= 1 << (button - 1)
+            else:
+                self._buttons_1_to_32_state &= ~(1 << (button - 1))
+        elif button < 65:
+            if value:
+                self._buttons_33_to_64_state |= 1 << (button - 33)
+            else:
+                self._buttons_33_to_64_state &= ~(1 << (button - 33))
+        else:
+            raise ValueError(f'Button {button} does not exist')
+        if send:
+            self.send()
+
+    def set_button(self, button: int, pressed: bool | int, send=False):
+        """
+        releases or presses button
+
+        button -- button number to press (starting at 1)
+
+        pressed -- if button is pressed
+
+        send -- if report should be send upon change
+        """
+        current_state = self._get_state_for_button(button)
+        if current_state and not pressed:
+            # print(f'Releasing button {button}')
+            self._set_state_for_button(
+                button=button,
+                value=False,
+                send=send
+            )
+        elif not current_state and pressed:
+            # print(f'Pressing button {button}')
+            self._set_state_for_button(
+                button=button,
+                value=True,
+                send=send
+            )
+
+    def click_button(self, button: int):
+        """
+        presses and releases button asap and sends state to host inbetween
+
+        NOTE: if button is already pressed, will not release beforehand
+
+        button -- button number to press (starting at 1)
+        """
+        self.set_button(
+            button=button,
+            pressed=True,
+            send=True
+        )
+        self.set_button(
+            button=button,
+            pressed=False,
+            send=True
+        )
+
+    def reset_all(self):
+        """
+        releases all buttons and sends report
+        """
+        self._buttons_1_to_32_state = 0
+        self._buttons_33_to_64_state = 0
+        self.send(force=True)
+
+    def send(self, force=False):
+        """
+        sends a report with all data (e.g. button states) to host
+
+        force -- send even if no changes present
+        """
+        struct.pack_into(
+            "<II",
+            self._report,
+            0,
+            self._buttons_1_to_32_state,
+            self._buttons_33_to_64_state
+        )
+
+        if force or self._last_report != self._report:
+            self._device.send_report(self._report)
+            # Remember what we sent, without allocating new storage.
+            self._last_report[:] = self._report
